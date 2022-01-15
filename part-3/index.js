909dc0709de8bbccc06dd9acdb836620fe7f1146
Dale Gordon <dbgordon09@gmail.com>
2022-01-15 17:48:55 +0200

    3.3: Phonebook backend step3

--- part-3/index.js
+++ part-3/index.js
@@ -32,9 +32,22 @@ app.get('/api/persons', (_req, res) => {
   res.json(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  console.log('ID!', id)
+  const person = persons.find((e) => e.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end('Person does not exist in phonebook');
+  }
+});
+
 app.get('/info', (_req, res) => {
-  res.send(`Phonebook has info for ${persons.length} people<br/><br/>${new Date()}`)
-})
+  res.send(
+    `Phonebook has info for ${persons.length} people<br/><br/>${new Date()}`
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
